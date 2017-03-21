cc8772f2bc4fa0dcb0a672de971359cfd6fb467b
tarique-s <tarique_s@hotmail.com>
2017-03-21 18:38:34 -0400

    some drop down list work

--- WebsiteRanking.App/wwwroot/js/controllers/websiteController.js
+++ WebsiteRanking.App/wwwroot/js/controllers/websiteController.js
@@ -2,9 +2,15 @@
 
 websitesApp.controller('websitesController',
     function websitesController($scope, websitesDataService) {
-
         $scope.visitDateList = websitesDataService.getVisitDates();
+        //$scope.selectedDate = $scope.visitDateList[0];
 
-        $scope.webistesVisitList = websitesDataService.getWebsitesVisitList('2016-01-13');
+        //$scope.webistesVisitList = websitesDataService.getWebsitesVisitList('2016-01-13');
+
+        $scope.getWebsitesList = function (selectedDate) {
+            if (selectedDate != '') {
+                $scope.webistesVisitList = websitesDataService.getWebsitesVisitList(selectedDate);
+            }
+        }
     }
 )
