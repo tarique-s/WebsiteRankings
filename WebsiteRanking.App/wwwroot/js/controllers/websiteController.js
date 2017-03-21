06e1eba21e5ca97e929d0317cad8410c99577c29
tarique-s <tarique_s@hotmail.com>
2017-03-20 22:34:27 -0400

    some more scripting

--- WebsiteRanking.App/wwwroot/js/controllers/websiteController.js
+++ WebsiteRanking.App/wwwroot/js/controllers/websiteController.js
@@ -1,7 +1,19 @@
 ﻿'use strict';
 
 websitesApp.controller('websitesController',
-    function ($scope) {
+    function websitesController($scope) {
         $scope.helloMessage = 'Hello Tarique';
+
+        $scope.webistesVisitList = [
+            { "website": "www.google.com.au", "visitCount": 151749278 },
+            { "website": "www.facebook.com", "visitCount": 104346720 },
+            { "website": "www.youtube.com", "visitCount": 59811438 },
+            { "website": "www.google.com", "visitCount": 26165099 },
+            { "website": "ninemsn.com.au", "visitCount": 21734381 },
+            { "website": "mail.live.com", "visitCount": 21536612 },
+            { "website": "www.ebay.com.au", "visitCount": 19831166 },
+            { "website": "www.bing.com", "visitCount": 14065457 },
+            { "website": "www.wikipedia.org", "visitCount": 13246531 },
+            { "website": "au.yahoo.com", "visitCount": 11492756 }]
     }
 )
