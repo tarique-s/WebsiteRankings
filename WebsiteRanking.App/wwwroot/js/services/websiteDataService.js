178883cd07e26052ed5b43338dff62e7e1a418e6
tarique-s <tarique_s@hotmail.com>
2017-03-21 11:40:34 -0400

    added service to the web app

---
+++ WebsiteRanking.App/wwwroot/js/services/websiteDataService.js
@@ -0,0 +1,18 @@
+﻿websitesApp.factory('websitesDataService', function () {
+    return {
+        webistesVisitList : [
+            { "website": "www.google.com.au", "visitCount": 151749278 },
+            { "website": "www.facebook.com", "visitCount": 104346720 },
+            { "website": "www.youtube.com", "visitCount": 59811438 },
+            { "website": "www.google.com", "visitCount": 26165099 },
+            { "website": "ninemsn.com.au", "visitCount": 21734381 },
+            { "website": "mail.live.com", "visitCount": 21536612 },
+            { "website": "www.ebay.com.au", "visitCount": 19831166 },
+            { "website": "www.bing.com", "visitCount": 14065457 },
+            { "website": "www.wikipedia.org", "visitCount": 13246531 },
+            { "website": "au.yahoo.com", "visitCount": 11492756 }
+        ],
+
+        visitDateList = ["2016-01-06", "2016-01-13", "2016-01-20", "2016-01-27"]
+    }
+})
